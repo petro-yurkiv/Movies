0a27a9d735f0d6819079057a03f93d8d7167e650
Ptero Yurkiv <petroyur12@gmail.com>
2024-05-30 04:23:17 -0700

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Petro Yurkiv on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
